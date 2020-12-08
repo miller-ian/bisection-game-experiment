cacee73841e20ee55d4dfd654159c2a09cc80574
qsimeon <35947758+qsimeon@users.noreply.github.com>
2020-12-07 21:34:23 -0500

    renamed parser to parser2 becasue of module name conflict in python

--- analyze.py
+++ analyze.py
@@ -10,7 +10,7 @@ import parser2
 import seaborn as sns
 
 # process results 
-print("getting data for experiment: ", parser2.directory)
+print("Getting data for experiment: ", parser2.directory)
 results = parser2.get_list()
 print("Attributes of a participant: ", 
       results[0].__dict__.keys())
@@ -20,10 +20,10 @@ first_guesses = []
 for participant in results:
     responses = participant.responses
     if responses:
-        first_guesses.append(responses[0])
+        resp = responses[0]
+        if resp != 'null':
+            first_guesses.append(int(resp))
 
 print(first_guesses)
 
 sns.distplot(first_guesses)
-
-first_guesses_int = [int(i) for i in first_guesses]
