1d0d8b546cf6c2661ba5ff168b6041b80a2e05c8
qsimeon <35947758+qsimeon@users.noreply.github.com>
2020-12-09 00:54:45 -0500

    edits to analysis files

--- analyze.py
+++ analyze.py
@@ -17,13 +17,47 @@ print("Attributes of a participant: ",
 
 # get the data for the first guess
 first_guesses = []
+# logical vector capturing binary search knowledge
+knows_binSearch = [] 
 for participant in results:
     responses = participant.responses
+    familiar = 1 if participant.binary_search_familiar else 0
     if responses:
         resp = responses[0]
         if resp != 'null':
             first_guesses.append(int(resp))
+            knows_binSearch.append(familiar)
 
+# check outputs
 print(first_guesses)
+print(knows_binSearch)
 
-sns.distplot(first_guesses)
+# # plot the first guess
+# sns.distplot(first_guesses)
+
+# optimal bisection search trace
+def bisection_search_optimal(secret, lower, upper):
+    """
+    Args:
+        secret (int): the secret number to be guessed.
+        lower (int): inclusive lower bound of interval.
+        upper (int): inclusive upper bound of interval.
+
+    Returns:
+        A list of the optimal bisection search trace.
+    """
+    trace = []
+    guess = (upper + lower)//2
+    while guess != secret:
+        trace.append(guess)
+        # update guess
+        if guess < secret: # too low
+            lower = guess
+        elif guess > secret: # too high
+            upper = guess 
+        guess = (upper + lower)//2
+    # append last guess (which must be the secret number)
+    trace.append(guess)
+    return trace
+    
+    
